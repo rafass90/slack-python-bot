feae657f448f3d73e0da8b82280e4428a0d0a89e
rafass90 <nascimento.90rafael@gmail.com>
2019-08-22 12:00:01 -0300

    log

--- bot.py
+++ bot.py
@@ -91,7 +91,7 @@ class Bot(object):
     def direct_message(self, slack_event):
         client = slack.WebClient(os.environ.get('token'))
 
-        openM = client.im_open('rafael9s9')
+        openM = client.im_open(user='rafael9s9')
         print(openM)
         #user = client.users_lookupByEmail(email='rafael9s9@hotmail.com')
         #print(user)
