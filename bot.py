4daa16264c915da1ce5d486958a73d85dcea5168
rafass90 <nascimento.90rafael@gmail.com>
2019-08-26 19:06:45 -0300

    log

--- bot.py
+++ bot.py
@@ -104,6 +104,6 @@ class Bot(object):
         # Post the onboarding message in Slack
         response = self.client.chat_postMessage(
             #as_user=True,
-            channel=slack_event['team_id'],
+            channel=slack_event['event']['channel'],
             text="It's a onboarding message"
         )
